9a86e8c25c365d1804dd370ceea61a6fade33e0d
Udit Sarkar <159702011+uditiitb@users.noreply.github.com>
2025-01-15 14:36:52 +0530

    Update SensorMetadata.js

--- models/SensorMetadata.js
+++ models/SensorMetadata.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const SensorMetadataSchema = new mongoose.Schema({
     sensorId: { type: String, required: true, unique: true },
+    type: {type: String,required:true},
     location: {
         lat: { type: Number, required: true },
         lng: { type: Number, required: true },
-    },
+    }
 });
 
 module.exports = mongoose.model('SensorMetadata', SensorMetadataSchema);
