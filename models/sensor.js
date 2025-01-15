af1487275109d6f1aca49b50ec9a148290cb0c89
Udit Sarkar <159702011+uditiitb@users.noreply.github.com>
2025-01-15 14:37:12 +0530

    Update sensor.js

--- models/sensor.js
+++ models/sensor.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const SensorSchema = new mongoose.Schema({
     sensorId: { type: String, required: true, unique: true },
     data: { type: Object, required: true },
+    distanceshistory: {
+        type: [
+            [Number, Number, Number, Date] // Array of arrays with x, y, z, time
+        ],
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Sensor', SensorSchema);
